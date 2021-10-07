a476ffad857419d71861aeb8b8461a40d2b748aa
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-10-07 11:17:12 +0200

    Implement increasing and decreasing light brightness

--- lib/boken_web/live/light_live.ex
+++ lib/boken_web/live/light_live.ex
@@ -3,7 +3,6 @@ defmodule BokenWeb.LightLive do
 
 	def mount(_params, _session, socket) do
 		socket = assign(socket, :brightness, 10)
-		IO.inspect(socket)
 		{:ok, socket}
 	end
 
@@ -15,15 +14,25 @@ defmodule BokenWeb.LightLive do
 		    <%= @brightness %>%
 		  </span>
 		</div>
-		<button phx-click="toggle">
-		  💡
-		</button>
+		<button phx-click="decrement">-</button>
+		<button phx-click="toggle">💡</button>
+		<button phx-click="increment">+</button>
     """
   end
-
-	def handle_event("toggle", _, socket) do
-		assign(socket, :brightness, 0)
+	def handle_event("toggle", _, %{assigns: %{last_brightness: last_brightness}}=socket) do
+		socket = %{socket | assigns: Map.delete(socket.assigns, :last_brightness)}
+		socket = assign(socket, :brightness, last_brightness)
 		{:noreply, socket}
 	end
-
+	def handle_event("toggle", _, socket) do
+		socket = assign(socket, :last_brightness, socket.assigns.brightness)
+		socket = assign(socket, :brightness, 0)
+		{:noreply, socket}
+	end
+	def handle_event("increment", _, socket) do
+		{:noreply, update(socket, :brightness, &(&1 + 10))}
+	end
+	def handle_event("decrement", _, socket) do
+		{:noreply, update(socket, :brightness, &(&1 - 10))}
+	end
 end
